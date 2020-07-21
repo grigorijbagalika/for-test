8b9e28f0a3ab24c1a9beedbd080ff4e4414fa3d6
grigorijbagalika <63339867+grigorijbagalika@users.noreply.github.com>
2020-07-21 20:40:30 +0300

    3

--- test.js
+++ test.js
@@ -8,3 +8,14 @@ car.update();
 function speedChange() {
   console.log(car.maxSpeed);
 }
+
+const age = 10;
+const totalPrice = 200.74;
+const userName = "Huck";
+const messege = "Hello world";
+const isOpen = true;
+const sholdConfirm = false;
+
+const type = typeof age;
+
+console.log(type);
